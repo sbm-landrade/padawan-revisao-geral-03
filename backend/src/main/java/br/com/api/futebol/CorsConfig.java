da2ff6a827ed9cad3a8e3588dec80066603df571
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-08-07 14:56:03 -0300

    Config e acesso ao banco

---
+++ backend/src/main/java/br/com/api/futebol/CorsConfig.java
@@ -0,0 +1,16 @@
+package br.com.api.futebol;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig  implements WebMvcConfigurer {
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas as rotas
+                .allowedOrigins("http://localhost:4200") // Permite requisições do frontend Angular
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite envio de cookies e credenciais
+    }
+
+}
